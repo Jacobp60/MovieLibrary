655e86b8bcacfdeb2112bc90c28a81adbfa2c7db
Jacobp60 <Perezj4801@gmail.com>
2022-09-21 01:58:50 -0500

    Code

---
+++ Services/IFileService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ApplicationTemplate.Services;
+
+/// <summary>
+///     This service interface only exists an example.
+///     It can either be copied and modified, or deleted.
+/// </summary>
+public interface IFileService
+{
+    public List<int> MovieIds { get; set; }
+
+    public List<string> MovieTitles { get; set; }
+
+    public List<string> MovieGenres { get; set; }
+
+    void Read();
+
+    void Write(int movieId, string movieTitle, string genresString);
+
+    void Display();
+}
